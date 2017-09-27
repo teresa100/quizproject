c39475298353cbf3d10fbe1112d51e52de73c97f
teresa100 <t_vikstedt@hotmail.com>
2017-09-27 12:55:05 +0200

    up to #15

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render
 
+quizzes = [
+{
+	"quiz_number": 1,
+	"name": "Klassiska böcker",
+	"description": "Hur bra kan du dina klassiker?"
+},
+{
+	"quiz_number": 2,
+	"name": "Största fotbollslagen",
+	"description": "Kan du dina lag?"
+},
+{	"quiz_number": 3,
+	"name": "Världens mest kända hackare",
+	"description": "Kan du din hackerhistoria?"
+},
+]
+
 # Create your views here.
 def startpage(request):
-	return render(request, "start.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "start.html", context)
 
 def quiz(request, quiz_number):
-	return render(request, "quiz.html")
+	context = {
+		"quiz": quizzes[quiz_number - 1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz.html", context)
 
 def question(request, quiz_number,question_number):
 	return render(request, "question.html")
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,7 +3,7 @@ from quiz import views
 
 urlpatterns = [
 	path("", views.startpage),
-	path("quiz/int:quiz_number>/", views.quiz),
+	path("quiz/<int:quiz_number>/", views.quiz),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
 	path("quiz/<int:quiz_number>/completed/", views.completed)
 
