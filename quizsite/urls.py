71390a61e4d607bad93e8541c198fdb8274b9c6e
teresa100 <t_vikstedt@hotmail.com>
2017-09-28 11:04:34 +0200

    created admin, login and changeable Quiz and Questions forms

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from quiz import views
 
@@ -5,6 +6,6 @@ urlpatterns = [
 	path("", views.startpage, name="start_page"),
 	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
-	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page")
-
+	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
+	path("admin/", admin.site.urls),
 	]
