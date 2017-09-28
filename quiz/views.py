74284a641e072bc93ecf8d0cb395da721ac5535d
teresa100 <t_vikstedt@hotmail.com>
2017-09-28 14:10:17 +0200

    added sessions, forms

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from quiz.models import Quiz
+from django.shortcuts import redirect
 
 # Create your views here.
 def startpage(request):
@@ -15,13 +16,17 @@ def quiz(request, quiz_number):
 	}
 	return render(request, "quiz.html", context)
 
-def question(request, quiz_number,question_number):
+def question(request, quiz_number, question_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = quiz.questions.all()
+	question = questions[question_number - 1]
 	context = {
 	"question_number": question_number,
-	"question":"Hur många bultar har ölandsbron?",
-	"answer1": "12",
-	"answer2": "66 400",
-	"answer3": "7 428 954",
+	"question": question.question,
+	"answer1": question.answer1,
+	"answer2": question.answer2,
+	"answer3": question.answer3,
+	"quiz": quiz,
 	"quiz_number": quiz_number,
 	}
 	return render(request, "question.html", context)
@@ -31,5 +36,13 @@ def completed(request,quiz_number):
 		"correct": 12,
 		"total" : 20,
 		"quiz_number": quiz_number,
+		"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "results.html", context)
+
+def answer(request, quiz_number, question_number):
+	answer = request.POST["answer"]
+	saved_answers = request.session.get(str(quiz_number), {})
+	saved_answers[question_number] = int(answer)
+	request.session[quiz_number] = saved_answers
+	return redirect("question_page", quiz_number, question_number + 1)	
