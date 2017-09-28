d06ad3f9846c5721d183e1d94f26a7a4d4306df6
teresa100 <t_vikstedt@hotmail.com>
2017-09-28 12:00:22 +0200

    added datatbase to startpage

--- quiz/views.py
+++ quiz/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-quizzes = [
-{
-	"quiz_number": 1,
-	"name": "Klassiska böcker",
-	"description": "Hur bra kan du dina klassiker?"
-},
-{
-	"quiz_number": 2,
-	"name": "Största fotbollslagen",
-	"description": "Kan du dina lag?"
-},
-{	"quiz_number": 3,
-	"name": "Världens mest kända hackare",
-	"description": "Kan du din hackerhistoria?"
-},
-]
+from quiz.models import Quiz
 
 # Create your views here.
 def startpage(request):
 	context = {
-		"quizzes": quizzes,
+		"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "start.html", context)
 
