6fba3a9899564ca81ac229ab10656046cc614c83
teresa100 <t_vikstedt@hotmail.com>
2017-09-29 09:55:39 +0200

    heroku

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yin#7p5x2%nlo0h-qfnsyr0(t=ttes73ri^&&hc0yc)oo@!-ca'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(
+            os.path.join(BASE_DIR, 'db.sqlite3')
+        )
+    )
 }
 
 
@@ -123,3 +125,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+STATIC_ROOT = 'staticfiles'
